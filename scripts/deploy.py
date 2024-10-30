5f160d529409fcd97af1bff6e8151d3015031295
c-protocol-team <c-protocol-team@protonmail.com>
2024-10-30 11:06:19 -0400

    latest

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -8,17 +8,17 @@ from brownie import WRWAToken
 import brownie
 
 def load_accounts():
-    admin_account = brownie.accounts.load(adm)
+    admin_account = brownie.accounts.load("hsk-admin")
 
     return admin_account
 
-def deploy_wrwatoken(admin_account, dev_account):
-    return admin_account.deploy(WRWAToken, dev_account)
+def deploy_wrwatoken(admin_account, decimals):
+    return admin_account.deploy(WRWAToken, decimals)
 
 def main():
     admin_account = load_accounts()
 
-    # deploy XCRAY contract
     wrwatoken = deploy_wrwatoken(admin_account, 18)
+    print(wrwatoken)
     
     
